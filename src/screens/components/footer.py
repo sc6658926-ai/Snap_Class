45d6859b093aa797584e81544f0502afb8eb545f
Shiva chauhan <sc6658926@gmail.com>
2026-07-11 16:22:09 +0530

    Update Teachers screen

--- src/screens/components/footer.py
+++ src/screens/components/footer.py
@@ -10,3 +10,15 @@ def footer_home():
     </div>
                 
      """, unsafe_allow_html= True)
+    
+
+def footer_dashboard():
+    st.markdown("""
+                
+    <div  style= "margin-top 5rem; display: flex; gap: 6px; justify-content: center; text-align: center">
+        <p style= "font-weight: bold; color: black;> created with ❤️ by Shiva_Chauhan </p>
+        
+    </div>
+                
+                
+        """, unsafe_allow_html= True)
