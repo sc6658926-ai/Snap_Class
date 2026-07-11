73f596dbc6bbd35a48cc602f2eb233c5cf1952bc
Shiva chauhan <sc6658926@gmail.com>
2026-07-11 13:05:55 +0530

    Initial commit

--- src/screens/Teachers_screen.py
+++ src/screens/Teachers_screen.py
@@ -1,9 +1,11 @@
 import streamlit as st
-from src.screens.ui.base_layout import style_backround_dashboard
+from src.screens.ui.base_layout import style_backround_dashboard, style_base_layout
 
 def Teachers_screen():
     
  style_backround_dashboard()
+ 
+ style_base_layout()
         
  st.header('Teacher_screen')
          
