7ba33dceaf7423a1d6b36ad92f50dfb2c6e4bca9
Shiva chauhan <sc6658926@gmail.com>
2026-07-24 23:25:42 +0530

    update Student screen

--- src/screens/database/db.py
+++ src/screens/database/db.py
@@ -32,6 +32,6 @@ def get_all_students():
     return response.data
 
 def create_student(new_name, face_embedding= None, voice_embedding= None):
-    data= {'name': new_name,'face_embedding': face_embedding, 'voice_embedding': voice_embedding}
+    data= {'name': new_name,"face_embedding": face_embedding, "voice_embedding": voice_embedding}
     response= supabase.table('students').insert(data).execute()
     return response.data
